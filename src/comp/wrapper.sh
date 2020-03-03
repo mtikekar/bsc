c8477ebb351d52b94195c89ecdac486873a3c0be
Mehul Tikekar <mehul.tikekar@analog.com>
2020-03-04 03:23:02 +0530

    remove use of realpath in wrapper.sh for MacOS

--- src/comp/wrapper.sh
+++ src/comp/wrapper.sh
@@ -1,13 +1,23 @@
 #! /usr/bin/env bash
 
-# Find the absolute name and location of this script
-#
-ABSNAME=$(realpath -e "$0")
-SCRIPTNAME=`basename "${ABSNAME}"`
-BINDIR=`dirname "${ABSNAME}"`
+# Find the absolute location of this script
+# https://gist.github.com/tvlooy/cbfbdb111a4ebad8b93e
+function abs_script_dir_path {
+    SOURCE=${BASH_SOURCE[0]}
+    while [ -h "$SOURCE" ]; do
+      DIR=$( cd -P $( dirname "$SOURCE") && pwd )
+      SOURCE=$(readlink "$SOURCE")
+      [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+    done
+    DIR=$( cd -P $( dirname "$SOURCE" ) && pwd )
+    echo $DIR
+}
+
+BINDIR="$(abs_script_dir_path)"
+SCRIPTNAME=`basename "${BASH_SOURCE[0]}"`
 
 # Set BLUESPECDIR based on the location
-BLUESPECDIR=$(realpath -e "${BINDIR}/../lib")
+BLUESPECDIR="${BINDIR}/../lib"
 export BLUESPECDIR
 
 # Add the dynamically-linked SAT libraris the load path
